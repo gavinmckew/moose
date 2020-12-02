121b4990ba656fa1d60e7168bad77c0df86461ba
Plug in Git <gitbot@plugingit.com>
2020-12-02 10:03:15 +0000

    Merge current Shopify theme changes

--- assets/custom.js
+++ assets/custom.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
 		//$(this).attr('data-aos', 'fade-up');
 	})
 
+	if($('.tabbed-section').length){
+		$('.rt-tab:first-child .rt-tab__radio').prop('checked', true);
+	}
+
 })
 
