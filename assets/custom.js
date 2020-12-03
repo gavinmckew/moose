21e3205e414353c1c9de5567af894884b2630bed
Plug in Git <gitbot@plugingit.com>
2020-12-03 15:03:35 +0000

    Merge current Shopify theme changes

--- assets/custom.js
+++ assets/custom.js
@@ -8,5 +8,39 @@ $(document).ready(function(){
 		$('.rt-tab:first-child .rt-tab__radio').prop('checked', true);
 	}
 
-})
+	$('.second-level').prepend('<li class="back">Back</li>');
+
+		var toggleLevel = function(e){
+			var l = e.data.level;
+			var $menu = $('menu');
+			// Toggele first level
+			if(l == 1){
+				if($(e.target).hasClass('js-menu') || ($('.is-open').length && !$(e.target).parents('nav').length))
+				$menu.toggleClass('is-open');
+			}
+			else if(l == 2){
+				// Close second level
+				if($menu.hasClass('is-first-level')){
+					$('.second-level, menu').removeClass('is-second-level is-first-level');
+				}
+				// Open second level
+				else{
+					$menu.addClass('is-first-level').find($(this)).next('.second-level').addClass('is-second-level');
+				}
+			}
+		}
+
+		/**
+			Toggle submenu
+		*/
+
+		$('.first-level li > a, .back').on("click", { level: 2 }, toggleLevel);
+
+		/**
+			Toggle menu
+		*/
+
+		$(document).on('click', { level: 1 }, toggleLevel);
+
+		})
 
